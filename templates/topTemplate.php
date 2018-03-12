197cff31669eaebe7bef7e8208bf393e679e7f7a
zacharia sao <zakaria.sao@gmail.com>
2018-03-12 16:44:56 +0100

    added link to homepage in navbar blablapz

--- templates/topTemplate.php
+++ templates/topTemplate.php
@@ -1,7 +1,7 @@
 <!-- Navigation -->
     <nav class="navbar navbar-light bg-light static-top">
       <div class="container">
-        <a class="navbar-brand" href="#">BlaBlaPZ</a>
+        <a class="navbar-brand" href="index.php">BlaBlaPZ</a>
         <a class="btn btn-primary" href="index.php?action=connexion">Mon Compte</a>
 		<a class="btn btn-secondary" href="index.php?action=inscription">Inscription</a>
       </div>
