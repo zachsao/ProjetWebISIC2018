8ed28e51d63869331c91b84ec7e0cd2e8450c1d5
Pierre <pierre.galus@live.fr>
2018-03-12 15:01:07 +0100

    #1 Add bootstrap connexion&inscriptionTemplate

--- templates/connexionTemplate.php
+++ templates/connexionTemplate.php
@@ -3,6 +3,7 @@
 <?php if(isset($inscErrorText)) 
 	echo '<span class="error">' . $inscErrorText . '</span>';
 ?>
+<!--
 <form action="index.php?action=validateConnexion" method="post"> 
 	<table>
 		<tr> 
@@ -18,4 +19,195 @@
 			<td><input type="submit" value="Connexion" /></td> 
 		</tr>
 	</table> 
-</form> 
+</form> -->
+
+
+	<style>
+		
+		h1{
+			color:DarkBlue;
+			text-align:center;
+		}
+		
+		h2{
+			color:White;
+			text-align:center;
+		}
+		
+		label{
+			color : rgb(40,40,40);
+			font-size:20px;
+			font-weight:bold;
+		}
+
+		#pageConnexion{
+			background-image:url(img/connexionWallpaper.jpg);
+			background-repeat: no-repeat;
+			background-size: cover;
+			min-height:100%;
+		}
+		
+		footer{
+			clear:both;
+		}
+		
+		.container{
+			margin:0px;
+			max-width:3000px;
+		}
+		
+		input{
+			width:120px;
+			margin:auto;
+			background-color: rgba(20,20,100,0.8);
+			border : 1px rgba(50,50,130,0.8);
+			border-radius:5%;
+			
+			
+			text-align:center;
+			color:white;
+			font-size:1.2em;
+		}
+		
+	</style>
+	
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-light bg-light static-top">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">BlablaWork</a>
+        <a class="btn" href="index.html">Retour</a>
+      </div>
+    </nav>
+
+    <!-- Masthead -->
+    <div id="pageConnexion" class="col-md-12">
+      
+	  <div class="container">
+	  
+		<div class="col-md-6 " style="display:inline-block">
+			<div class="row">
+				</br>
+				<div class="col-md-12 "> <h1>Inscription</h1> </div>
+			</div>
+			</br></br></br>
+			
+			<form class="form-horizontal col-md-12 ">
+				
+				<div class="row">
+					<div class="form-group col-md-12 ">
+						<label for="text" class="col-md-3 control-label" style="display:inline-block">Login : </label>
+						<div class="col-md-8" style="display:inline-block">
+							<input type="text" class="form-control" placeholder="Darkou" name="inscLogin">
+						</div>
+					</div>
+                </div>
+				
+				<div class="row">
+					<div class="form-group col-md-12 ">
+						<label for="text" class="col-md-3 control-label" style="display:inline-block">Mot de passe :</label>
+						<div class="col-md-8" style="display:inline-block">
+							<input type="password" class="form-control" placeholder="JeSuisTonPere (au moins 8 caractères)..." name="inscPassword">
+						</div>
+					</div>
+                </div>
+				
+				<div class="row">
+					<div class="form-group col-md-12 ">
+						<label for="text" class="col-md-3 control-label" style="display:inline-block">Confirmer le mot de passe :</label>
+						<div class="col-md-8" style="display:inline-block">
+							<input type="password" class="form-control" placeholder="Entrer de nouveau votre mot de passe...">
+						</div>
+					</div>
+                </div>
+				
+				<div class="row">
+					<div class="form-group col-md-12 ">
+						<label for="text" class="col-md-3 control-label" style="display:inline-block">Mail : </label>
+						<div class="col-md-8" style="display:inline-block">
+							<input type="text" class="form-control" placeholder="dark-vador@imt-lille-douai.fr" name="mail">
+						</div>
+					</div>
+                </div>
+				
+				<div class="row">
+					<div class="form-group col-md-12 ">
+						<label for="text" class="col-md-3 control-label" style="display:inline-block">Nom :</label>
+						<div class="col-md-8" style="display:inline-block">
+							<input type="text" class="form-control" placeholder="Vador" name="nom">
+						</div>
+					</div>
+                </div>
+				
+				<div class="row">
+					<div class="form-group col-md-12 ">
+						<label for="text" class="col-md-3 control-label" style="display:inline-block">Prénom :</label>
+						<div class="col-md-8" style="display:inline-block">
+							<input type="text" class="form-control" placeholder="Dark" name="prenom">
+						</div>
+					</div>
+                </div>
+				
+				
+				<h2> Pour les élèves : </h2>
+				
+				<div class="row">
+					<div class="form-group col-md-12 ">
+						<label for="text" class="col-md-3 control-label" style="display:inline-block">Promo :</label>
+						<div class="col-md-8" style="display:inline-block">
+							<input type="text" class="form-control" placeholder="Entrer votre année de promotion...">
+						</div>
+					</div>
+                </div>
+				
+				<h2> Pour les agents : </h2>
+				
+				<div class="row">
+					<div class="form-group col-md-12 ">
+						<label for="text" class="col-md-3 control-label" style="display:inline-block">Service : </label>
+						<div class="col-md-8" style="display:inline-block">
+							<input type="text" class="form-control" placeholder="Entrer le service auquel vous appartenez...">
+						</div>
+					</div>
+                </div>
+				
+				<input type="submit" value="Inscription" />
+				<!--<a href="#" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-check"></span> S'incrire</a>
+				 https://bootsnipp.com/buttons -->
+				
+            </form>
+			
+		</div>
+		
+		<div class="col-md-5" style="display:inline-block; vertical-align:top;">
+			<div class="row">
+				</br>
+				<div class="col-md-12"> <h1>Déja inscrit ?</h1> </div>
+			</div>
+			</br></br></br></br></br></br>
+			
+			<form action="index.php?action=validateConnexion" class="form-horizontal col-md-12 ">
+				<div class="row">
+					<div class="form-group col-md-12 ">
+						<label for="text" class="col-md-3 control-label" style="display:inline-block">Mail : </label>
+						<div class="col-md-8" style="display:inline-block">
+							<input type="text" class="form-control" placeholder="Entrer votre email..." name="inscLogin">
+						</div>
+					</div>
+                </div>
+				
+				<div class="row">
+					<div class="form-group col-md-12 ">
+						<label for="text" class="col-md-3 control-label" style="display:inline-block">Mot de passe :</label>
+						<div class="col-md-8" style="display:inline-block">
+							<input type="password" class="form-control" placeholder="Entrer votre mot de passe..." name="inscPassword">
+						</div>
+					</div>
+                </div>	
+				<input type="submit" value="Connexion" />
+				<!--<a href="#" class="pull-right btn btn-lg btn-primary"><span class="glyphicon glyphicon-circle-arrow-right"></span> Se connecter</a>-->
+            </form>
+		</div>
+		</br></br></br>
+	</div>
+</div>
