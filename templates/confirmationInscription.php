539dac5b21450a5dcb0e1c891fc6d7770e2b7f3f
Pierre <pierre.galus@live.fr>
2018-03-30 11:25:48 +0200

    Add confirmation page

---
+++ templates/confirmationInscription.php
@@ -0,0 +1,51 @@
+
+	<style>
+
+	
+		#confirmationTrajet{
+			background-image:url(img/wallpaperConfirmationTrajet.jpg) ;
+			background-repeat: no-repeat;
+			background-size:cover;
+			background-position: top;
+			
+			min-height:95vh;
+		}
+		
+		#confirmationTrajet li{
+			font-style:italic;
+			font-size:1.1em;
+		}
+	
+	
+	
+	</style>
+	
+
+
+	
+	<div class="container col-12" id="confirmationTrajet">
+		<br><br>
+		<div class="card bg-light w-50">
+		  <div class="card-body">
+			<h2 class="card-title"><span class="glyphicon glyphicon-ok-circle"></span>Confirmation de votre trajet</h2>
+			<div class="card-text">
+				<div class="row col-12">
+					<p>Félicititation ! Vous vous êtes inscrit au trajet décrit ci-dessous.</br>Recapitulatif de votre trajet </p>
+				</div>
+				<div class="row offset-1 col-11">
+					<ul class="list-unstyled">
+						<li>Date de départ : </li>
+						<li>Lieu de départ : </li>
+						<li>Lieu d'arrivée : </li>
+						<li>Nom du pilote : </li>
+					</ul>
+				</div>	
+			</div>
+			<a href="#" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Retour à l'accueil</a>
+			<a href="#" class="btn btn-success pull-right"><span class="glyphicon glyphicon-road"></span> Voir mes trajets</a>
+		  </div>
+		</div>
+	</div>
+	
+
+
